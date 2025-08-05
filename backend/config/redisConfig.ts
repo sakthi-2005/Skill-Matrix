e5e3ef2da4017e59abb9b332033599d398baa675
HARISH K <138508035+dapphari007@users.noreply.github.com>
2025-08-05 15:27:55 +0530

    Update redisConfig.ts

--- backend/config/redisConfig.ts
+++ backend/config/redisConfig.ts
@@ -5,7 +5,7 @@ export const config = {
   connection: { 
         port: parseInt(process.env.REDIS_PORT),
         host: process.env.REDIS_HOST,
-        tls: {},
+        ssl: {},
         password: process.env.REDIS_PASSWORD,
         maxRetriesPerRequest: null 
     },
